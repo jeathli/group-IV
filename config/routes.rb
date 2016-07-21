db3dff7c45b6486e88665f7ab97069797ba9814c
basia <basiacierocka@gmail.com>
2016-07-21 16:30:28 +0200

    [#19] create recipe page and page with search results (#50)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
   get 'home/index'
 
   root 'home#index'
+
+  get 'recipes/show'
+
+  get 'recipes' => "recipes#index"
+
+  get '/' => 'application#index'
+
 end
