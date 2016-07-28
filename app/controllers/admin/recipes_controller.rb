fa0d8301b1060164ae9980702e7386b2660a7867
Aneta Wozniak <wozniakaneta5@gmail.com>
2016-07-28 12:24:32 +0200

    admin base controller+layout (#94)

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,5 +1,4 @@
-class Admin::RecipesController < ApplicationController
-  http_basic_authenticate_with name: ENV.fetch('ADMIN_USER_NAME', 'admin'), password: ENV.fetch('ADMIN_PASSWORD', '1234')
+class Admin::RecipesController < Admin::BaseController
 
   def index
     @recipes = Recipe.all.order(:name)
